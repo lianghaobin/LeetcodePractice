183a07932b7a248b9ca67e89bee31db38a73008b
lianghaobin <19802021582@139.com>
2021-04-20 21:39:44 +0800

    first commit

---
+++ leetcode/editor/cn/P7ReverseInteger .java
@@ -0,0 +1,47 @@
+//Given a signed 32-bit integer x, return x with its digits reversed. If reversi
+//ng x causes the value to go outside the signed 32-bit integer range [-231, 231 -
+// 1], then return 0. 
+//
+// Assume the environment does not allow you to store 64-bit integers (signed or
+// unsigned). 
+//
+// 
+// Example 1: 
+// Input: x = 123
+//Output: 321
+// Example 2: 
+// Input: x = -123
+//Output: -321
+// Example 3: 
+// Input: x = 120
+//Output: 21
+// Example 4: 
+// Input: x = 0
+//Output: 0
+// 
+// 
+// Constraints: 
+//
+// 
+// -231 <= x <= 231 - 1 
+// 
+// Related Topics 数学 
+// 👍 2709 👎 0
+
+
+//Java：Reverse Integer
+class P7ReverseInteger {
+    public static void main(String[] args) {
+        Solution solution = new P7ReverseInteger().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int reverse(int x) {
+            return 1;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
